41debd5515e50a5b0d612100357b8817726ee935
Josef Butts <jbutts@utexas.edu>
2017-05-27 09:27:25 -0500

    initial commit

---
+++ pettiford.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+
+<!--TODO fix scaling from toggle side bar, either make it cover instead of pushing, or fix scaling-->
+<!--TODO can the footnotes pop up in a bottom bar, pop-up window, sidebar or bubble?-->
+<head>
+  <meta name=Title content="Oscar Pettiford: The Prototypical Modern Bass soloist">
+  <meta name=Keywords
+  content="Oscar Pettiford, Josef Butts, jazz bass, upright bass, bebop, 52nd street, dizzp gillespie, max roach, chalres mingus, paul chambers, ray brown, bass solos, new york jazz">
+  <meta http-equiv=Content-Type content="text/html; charset=macintosh">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <link href='style.css' rel='stylesheet' type='text/css'>
+  <script src="pettiford.js"></script>
+
+
+
+
+</head>
+
+<body>
+  <header>
+    <div class="row">
+
+        <div class="container-fluid" id="navbar">
+          <div id="mySidenav" class="sidenav">
+            <ul>
+              <li>
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#title-page">Title Page</a>
+                <a href="#acknowledgements">Acknowledgements and Notes</a>
+                <a href="#list-of-examples"> List of Examples</a><!--TODO name each exampl and link-->
+                <a href="#chapter-one-background">1. Background</a>
+                <a href="#chapter-two-analysis-of-pettifords-solos">2. Analysis of Pettiford's Solos</a>
+                <a href="#chapter-three-analysis-of-pettifords-peers">3. Analysis of Pettiford's Peers</a>
+                <a href="#chapter-four-analysis-of-three-modern-bass-soloists">4. Analysis of Three Modern Bass Soloists</a>
+                <a href="#conclusion">Conclusion</a>
+                <a href="#appendix-transcriptions">Appendix:Transcriptions</a>
+                <a href="#bibliography">Bibliography</a>
+                <a href="#pdf-version">PDF Version</a>
+              </li>
+            </ul>
+          </div>
+
+          <!-- Use any element to open the sidenav -->
+          <div class="pull-left" id="nav-button">
+            <span onclick="openNav()">
+              <button type="button" class="btn btn-default btn-lg">
+                      <span class="glyphicon glyphicon-th-list"> Sections</span>
+                    </button>
+            </span>
+          </div>
+
+        </div>
+
+        <h2>Oscar Pettiford: The Prototypical Bass Soloist</h2>
+        <h4>By <a href="#">Josef Butts</a> Doctor of Musical Arts</h4>
+
+      </div>
+
+    </div><!--row-->
+  </header>
+
+<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+<main id="main">
+
+        <section id="title-page">
+          <a name="Title Page"></a>
+          <br />
+          <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+
+          <p>Oscar Pettiford: The Prototypical Bass Soloist</p>
+          <p>By <a href="#">Josef Butts</a> Doctor of Musical Arts</p>
+          <br />
+          <br />
+
+
+
+          <p>Webpage copyright &copy 2017, <br />
+            Original Document &copy 2011
+          Josef Andrew Butts</p>
+
+          <p>All rights reserved</p>
+          <br /><br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+        
+          <p style="page-break-after:always;"></p>
+        </section><!-- title-page-->
+        <br />
+        <hr />
+        <section id="acknowledgements">
+        <a name="acknowledgements"></a>
+          <div ="container-fluid">
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <h3><strong>Acknowledgements and Notes</strong></h3>
+            <br/>
+
+            <!--TODO ADD LINK TO PDF DOWNLOAD--TODO ADD LINK PAGE FOR OTEHR JAZZ BASS DISSERTATIONS-->
+            <p>   This document was originally researched, written and defended as the final step of the Doctor of Musical Arts Degree
+            program in Jazz Performance at the University of Texas at Austin's Butler School of Music. It has not been made easily accessible from the UT Austin library's online repository, so I have decided
+            to make it public through this website. It is presented largely in it's original text, save for additions to these acknowledgements and the formatting neccessary
+            to effectively present it via web browsers. It was also done as an exercise in learning to code an easy to navigate site featuring complex text, graphics, and a retracting navigation menu. This site was designed and contructed by myself using HTML5, CSS, Bootstrap and Javascript.
+            The source text was exported from Microsoft Word and reformatted to html first by Word, and then spot adjusted manually, which will be apparent from viewing the source code. The original document can be found <a href="#">here</a> as a pdf file. All solos were transcribed by myself,
+            and, at times present minor differences from previously published transcriptions of Oscar Pettiford.</p>
+
+            </p>
+            <br />
+            <!-- TODO add contact form/email link-->
+            <p>Please drop me a message via the <a href="#">contact form </a>if you find this document interesting or useful in your personal or academic research. As mentioned above, a pdf is available for download.
+              I look forward to hearing from you! If you have a paper or resources that you would like featured on the <a href="#">related links</a> page, please forward it to me.</p>
+            <br />
+            <!--TODO add the last members name-->
+          <p>I would like to take this opportunity to thank my professors and the members of my commitee: <a href="#">John Fremgen</a>, <a href="#">Jeff Hellmer,</a>
+            <a href="#">Dr. John Mills</a>, <a href="#">Dr. David Neumeyer </a>, and <a href="#">Dr. Laurie Scott</a>. Each one of them pushed me to become a better writer, musician, student, and teacher. Additional, I have to thank all of my fellow students
+          , who were immensely talented, and inspired me to work harder than ever to keep up.</p>
+            <br />
+            <p>This document and web publication is dedicated to my family, Lissadell, Emma, Neave and Josefine.
+              They endured the many sacrifices of time and resources required to complete this document and the DMA program.</p>
+              <br />
+
+          </div>
+        </section>
+        <p style="page-break-after:always;"></p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <hr />
+
+        <br /><br />
+        <br />
+        <br />
+        <br />
+<!--
+        <?php
+      include '/list-of-contents.html';
+      include '/pettiford-chapter-one.html';
+      include '/pettiford-chapter-two.html';
+      include '/pettiford-chapter-three.html';
+      include '/pettiford-chapter-four.htm';
+      include '/pettiford-conclusion.html';
+      include '/bibliography.html';
+
+      ?>
+    -->
